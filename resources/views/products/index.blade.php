9f602b88da756ed4f06d33d77b9a9d77eb722d87
Facundo Enriquez <fenriquez@inym.org.ar>
2024-10-21 13:53:53 -0300

    tdd laravel daily first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,49 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Products') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700">
+                            <thead>
+                            <tr>
+                                <th class="px-6 py-3 text-left text-sm font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th class="px-6 py-3 text-left text-sm font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                    Price
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="bg-white dark:bg-gray-900">
+                            @forelse($products as $product)
+                                <tr class="border-b border-gray-200 dark:border-gray-700">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-200">
+                                        {{$product->name}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-200">
+                                        ${{$product->price}}
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr class="border-b border-gray-200 dark:border-gray-700">
+                                    <td colspan="2" class="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-200">
+                                        {{__('No products found')}}
+                                    </td>
+                                </tr>
+                            @endforelse
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
