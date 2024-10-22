b7207c3e769c861402ea910e80e2648a54daf081
Facundo Enriquez <fenriquez@inym.org.ar>
2024-10-22 13:53:41 -0300

    testing products with factories

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -9,6 +9,9 @@ use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     public function test_homepage_contains_empty_table(): void
     {
         $response = $this->get('/products');
@@ -19,7 +22,7 @@ class ProductsTest extends TestCase
 
     public function test_homepage_contains_non_empty_table(): void
     {
-        Product::create([
+        $product = Product::create([
             'name' => 'Product 1',
             'price' => 123
         ]);
@@ -28,5 +31,21 @@ class ProductsTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertDontSee(__('No products found'));
+        $response->assertSee('Product 1');
+        $response->assertViewHas('products', function ($collection) use ($product) {
+            return $collection->contains($product);
+        });
+    }
+
+    public function test_paginated_products_table_doesnt_contain_11th_record()
+    {
+        $products = Product::factory(11)->create();
+        $lastProduct = $products->last();
+        $response = $this->get('/products');
+        $response->assertStatus(200);
+
+        $response->assertViewHas('products', function ($collection) use ($lastProduct) {
+            return $collection->contains($lastProduct);
+        });
     }
 }
